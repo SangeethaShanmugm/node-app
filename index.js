f264ddf0d67afc7a988df7730d3c19380b9bcb08
Sangeetha <sanjuriya22@gmail.com>
2021-08-29 22:28:40 +0530

    first process.argv

---
+++ index.js
@@ -0,0 +1,48 @@
+console.log("Hi Sangee is a software developer");
+
+const users=[
+    {
+     "createdAt": "2021-08-27T02:16:01.017Z",
+     "name": "Roger Bayer",
+     "avatar": "https://cdn.fakercloud.com/avatars/polarity_128.jpg",
+     "id": "1"
+    },
+    {
+     "createdAt": "2021-08-27T01:04:32.249Z",
+     "name": "Nellie Jenkins",
+     "avatar": "https://cdn.fakercloud.com/avatars/ecommerceil_128.jpg",
+     "id": "2"
+    },
+    {
+     "createdAt": "2021-08-26T17:21:02.859Z",
+     "name": "Hilda Harber",
+     "avatar": "https://cdn.fakercloud.com/avatars/eyronn_128.jpg",
+     "id": "3"
+    },
+    {
+     "createdAt": "2021-08-26T14:27:00.864Z",
+     "name": "Kelley Howe",
+     "avatar": "https://cdn.fakercloud.com/avatars/matthewkay__128.jpg",
+     "id": "4"
+    },
+    {
+     "createdAt": "2021-08-26T21:30:55.783Z",
+     "name": "Leigh Windler",
+     "avatar": "https://cdn.fakercloud.com/avatars/rohixx_128.jpg",
+     "id": "5"
+    },
+    {
+     "createdAt": "2021-08-26T18:32:52.316Z",
+     "name": "Lloyd Feeney",
+     "avatar": "https://cdn.fakercloud.com/avatars/mactopus_128.jpg",
+     "id": "6"
+    },
+    {
+     "createdAt": "2021-08-26T18:07:52.914Z",
+     "name": "Shawna Dickinson",
+     "avatar": "https://cdn.fakercloud.com/avatars/emmandenn_128.jpg",
+     "id": "7"
+    }
+   ]
+
+   console.log(users.map(user =>user.name))
