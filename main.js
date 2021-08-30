5a7dc41a15543f46d2f77834287597f4d72f4ba4
Sangeetha <sanjuriya22@gmail.com>
2021-08-30 19:36:00 +0530

    First commit

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-console.log("Code in this file");
+console.log("Code in this file",process.argv);
+const arr=process.argv[2];
+// const arr=[5,6,10,40,3];
+console.log(arr,typeof arr);
 
-console.log("Max number is: ");
+//1st method
+// let max=arr[0];
+
+// arr.forEach((num)=>
+// {
+//     if(max<num)  {
+//         max=num;  }
+// })
+// console.log("Max number is: ",max);
+
+// 2nd method in one line
+
+console.log(Math.max(...arr));
