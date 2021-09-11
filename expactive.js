f4ea704eb6055c390b084e4d14973ccbaa523e02
Sangeetha <sanjuriya22@gmail.com>
2021-09-11 15:29:21 +0530

    mongoatleas cloud connected

--- expactive.js
+++ expactive.js
@@ -1,53 +1,71 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 const PORT=5000;
 
-const user=[
+// const user=[
     
-        {
-         "createdAt": "2021-08-27T02:16:01.017Z",
-         "name": "Roger Bayer",
-         "avatar": "https://cdn.fakercloud.com/avatars/polarity_128.jpg",
-         "id": "1"
-        },
-        {
-         "createdAt": "2021-08-27T01:04:32.249Z",
-         "name": "Nellie Jenkins",
-         "avatar": "https://cdn.fakercloud.com/avatars/ecommerceil_128.jpg",
-         "id": "2"
-        },
-        {
-         "createdAt": "2021-08-26T17:21:02.859Z",
-         "name": "Hilda Harber",
-         "avatar": "https://cdn.fakercloud.com/avatars/eyronn_128.jpg",
-         "id": "3"
-        },
-        {
-         "createdAt": "2021-08-26T14:27:00.864Z",
-         "name": "Kelley Howe",
-         "avatar": "https://cdn.fakercloud.com/avatars/matthewkay__128.jpg",
-         "id": "4"
-        },
-        {
-         "createdAt": "2021-08-26T21:30:55.783Z",
-         "name": "Leigh Windler",
-         "avatar": "https://cdn.fakercloud.com/avatars/rohixx_128.jpg",
-         "id": "5"
-        },
-        {
-         "createdAt": "2021-08-26T18:32:52.316Z",
-         "name": "Lloyd Feeney",
-         "avatar": "https://cdn.fakercloud.com/avatars/mactopus_128.jpg",
-         "id": "6"
-        },
-        {
-         "createdAt": "2021-08-26T18:07:52.914Z",
-         "name": "Shawna Dickinson",
-         "avatar": "https://cdn.fakercloud.com/avatars/emmandenn_128.jpg",
-         "id": "7"
-        }
+//         {
+//          "createdAt": "2021-08-27T02:16:01.017Z",
+//          "name": "Roger Bayer",
+//          "avatar": "https://cdn.fakercloud.com/avatars/polarity_128.jpg",
+//          "id": "1"
+//         },
+//         {
+//          "createdAt": "2021-08-27T01:04:32.249Z",
+//          "name": "Nellie Jenkins",
+//          "avatar": "https://cdn.fakercloud.com/avatars/ecommerceil_128.jpg",
+//          "id": "2"
+//         },
+//         {
+//          "createdAt": "2021-08-26T17:21:02.859Z",
+//          "name": "Hilda Harber",
+//          "avatar": "https://cdn.fakercloud.com/avatars/eyronn_128.jpg",
+//          "id": "3"
+//         },
+//         {
+//          "createdAt": "2021-08-26T14:27:00.864Z",
+//          "name": "Kelley Howe",
+//          "avatar": "https://cdn.fakercloud.com/avatars/matthewkay__128.jpg",
+//          "id": "4"
+//         },
+//         {
+//          "createdAt": "2021-08-26T21:30:55.783Z",
+//          "name": "Leigh Windler",
+//          "avatar": "https://cdn.fakercloud.com/avatars/rohixx_128.jpg",
+//          "id": "5"
+//         },
+//         {
+//          "createdAt": "2021-08-26T18:32:52.316Z",
+//          "name": "Lloyd Feeney",
+//          "avatar": "https://cdn.fakercloud.com/avatars/mactopus_128.jpg",
+//          "id": "6"
+//         },
+//         {
+//          "createdAt": "2021-08-26T18:07:52.914Z",
+//          "name": "Shawna Dickinson",
+//          "avatar": "https://cdn.fakercloud.com/avatars/emmandenn_128.jpg",
+//          "id": "7"
+//         }
        
-   ]
+//    ]
+
+
+async function createConnection(){
+    const MONGO_URL = "mongodb+srv://sangeetha:sangeetha@cluster0.scjhy.mongodb.net/contestants?retryWrites=true&w=majority";
+    const client = new MongoClient(MONGO_URL) ;
+
+    try{
+        await client.connect();
+        console.log("Successfully Connected");
+    }catch(err){
+        console.log(err);
+    }
+    }
+
+createConnection();
+
+//Todo
 
 app.get("/", (request, response)=>{
     response.send("Hello I am a Senior Developer in Google USA");
